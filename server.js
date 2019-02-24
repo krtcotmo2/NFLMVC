76f1c482733a47b039964424384ea9216b791007
Kurt Cooney <krtcotmo2@gmail.com>
2019-02-24 10:06:47 -0500

    still playing fit root static

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require("express");
 const path = require("path");
 const log = console.log;
 let PORT = process.env.PORT || 8080;
+const routes = require('./controllers/draftController');
 
 let app = express();
 app.use(express.urlencoded( {extended:true} ));
 app.use(express.json());
-app.use(express.static(__dirname +"/public"));
+app.use("/public", express.static(__dirname +"/public"));
 
 
 const exphbs = require("express-handlebars");
@@ -14,7 +15,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 
-let routes = require('./controllers/draftController');
+
 app.use("/", routes);
 
 
