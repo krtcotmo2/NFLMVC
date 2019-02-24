ba9b3c9214695a20853ef9afb63a0773abc6a669
Kurt Cooney <krtcotmo2@gmail.com>
2019-02-24 10:30:46 -0500

    static

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const express = require("express");
 const path = require("path");
 const log = console.log;
 let PORT = process.env.PORT || 8080;
-const routes = require('./controllers/draftController');
+const routes = require('./controllers/draftController,js');
 const exphbs = require("express-handlebars");
 let app = express();
 
 app.use(express.urlencoded( {extended:true} ));
 app.use(express.json());
-app.use(express.static("public"));
-app.use("/", routes);
-//app.use("/", express.static(__dirname + "/"));
+//app.use(express.static("public"));
 //app.use("/", routes);
+app.use("/", express.static(__dirname + "/"));
+app.use("/", routes);
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
