bca2fadb5ebdff4ef0b05c8c77d5d9d39d084ee6
Kurt Cooney <krtcotmo2@gmail.com>
2019-02-24 10:49:28 -0500

    static

--- controllers/draftController.js
+++ controllers/draftController.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-
+const path = require("path");
+connection = require("../config/connection");
 //add player model so that the api calls can update the data
 const player = require("../models/player");
 
