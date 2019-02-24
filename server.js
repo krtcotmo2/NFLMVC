ab78105773f153646eebb78e781f2ee48ec5f431
Kurt Cooney <krtcotmo2@gmail.com>
2019-02-24 10:55:05 -0500

    static

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ let app = express();
 app.use(express.urlencoded( {extended:true} ));
 app.use(express.json());
 app.use(express.static("public"));
-app.use("public", routes);
+app.use("/", routes);
 //app.use("/", express.static(__dirname + "/"));
 //app.use("/", routes);
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
